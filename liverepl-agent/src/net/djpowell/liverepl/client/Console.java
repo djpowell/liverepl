9f40304bf65da3976624f8cfb22742a62a11a5ef
David Powell <djpowell@djpowell.net>
2009-10-19 00:17:57 +0100

    tidied up formatting

--- liverepl-agent/src/net/djpowell/liverepl/client/Console.java
+++ liverepl-agent/src/net/djpowell/liverepl/client/Console.java
@@ -13,70 +13,70 @@ import java.util.logging.Logger;
 
 public class Console {
 
-	private static String NEWLINE = System.getProperty("line.separator");
-	
-	public static void main(InetAddress host, int port) throws Exception {
-		final BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-		final Writer cout = new OutputStreamWriter(System.out);
-		Socket s = new Socket(host, port);
-		try {
-			final Thread main = Thread.currentThread();
-			final Reader sin = new InputStreamReader(s.getInputStream());
-			final Writer sout = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+    private static String NEWLINE = System.getProperty("line.separator");
+    
+    public static void main(InetAddress host, int port) throws Exception {
+        final BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+        final Writer cout = new OutputStreamWriter(System.out);
+        Socket s = new Socket(host, port);
+        try {
+            final Thread main = Thread.currentThread();
+            final Reader sin = new InputStreamReader(s.getInputStream());
+            final Writer sout = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
 
-			Thread ch = new Thread("ConsoleHandler") {
-				@Override
-				public void run() {
-					try {
-						for (;;) {
-							// use line-based i/o for reading from the keyboard
-							String line = cin.readLine();
-							if (line == null) break;
-							sout.write(line + NEWLINE);
-							sout.flush();
-						}
-					} catch (IOException e) {
-						TRC.fine(e.getMessage());
-					} finally {
-						main.interrupt();
-					}
-				}
-			};
-			ch.start();
+            Thread ch = new Thread("ConsoleHandler") {
+                @Override
+                public void run() {
+                    try {
+                        for (;;) {
+                            // use line-based i/o for reading from the keyboard
+                            String line = cin.readLine();
+                            if (line == null) break;
+                            sout.write(line + NEWLINE);
+                            sout.flush();
+                        }
+                    } catch (IOException e) {
+                        TRC.fine(e.getMessage());
+                    } finally {
+                        main.interrupt();
+                    }
+                }
+            };
+            ch.start();
 
-			Thread sh = new Thread("SocketHandler") {
-				@Override
-				public void run() {
-					try {
-						for (;;) {
-							// use character based i/o for printing server responses
-							int ic = sin.read();
-							if (ic == -1) break;
-							char c = (char) ic;
-							cout.write(c);
-							cout.flush();
-						}
-					} catch (IOException e) {
-						TRC.fine(e.getMessage());
-					} finally {
-						main.interrupt();
-					}
-				}
-			};
-			sh.start();
-		
-			// block until both threads finish
-			try {
-				sh.join();
-				ch.join();
-			} catch (InterruptedException e) {
-				s.close();
-			}
-		} finally {
-			s.close();
-		}
-	}
-	
-	private static final Logger TRC = Logger.getLogger(Console.class.getName()); 
+            Thread sh = new Thread("SocketHandler") {
+                @Override
+                public void run() {
+                    try {
+                        for (;;) {
+                            // use character based i/o for printing server responses
+                            int ic = sin.read();
+                            if (ic == -1) break;
+                            char c = (char) ic;
+                            cout.write(c);
+                            cout.flush();
+                        }
+                    } catch (IOException e) {
+                        TRC.fine(e.getMessage());
+                    } finally {
+                        main.interrupt();
+                    }
+                }
+            };
+            sh.start();
+        
+            // block until both threads finish
+            try {
+                sh.join();
+                ch.join();
+            } catch (InterruptedException e) {
+                s.close();
+            }
+        } finally {
+            s.close();
+        }
+    }
+    
+    private static final Logger TRC = Logger.getLogger(Console.class.getName()); 
 
 }
