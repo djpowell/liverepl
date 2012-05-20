3b54ce774dfa79b161377f46ea93d6b3957f7dad
David Powell <djpowell@djpowell.net>
2012-05-20 10:10:34 +0100

    update project files

--- liverepl-server/project.clj
+++ liverepl-server/project.clj
@@ -1,6 +1,6 @@
 (defproject net.djpowell/liverepl-server "1.1.0"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :source-path "src/clj"
   :java-source-path "src/java"
   :javac-options {:debug "true"}
--- liverepl/project.clj
+++ liverepl/project.clj
@@ -1,6 +1,7 @@
 (defproject net.djpowell/liverepl "1.1.0"
   :description "FIXME: write description"
-  :dependencies [[net.djpowell/liverepl-agent "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [net.djpowell/liverepl-agent "1.1.0"]
                  [net.djpowell/liverepl-client "1.1.0"]
                  [net.djpowell/liverepl-server "1.1.0"]]
   )
