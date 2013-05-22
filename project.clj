0659da4ae6e936f86778ec6e2f4cceecbbd3ba46
Alexey Pushkin <pushkin@haulmont.com>
2013-05-22 16:52:51 +0400

    Added nrepl to dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject liverepl "1.2.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [clojure-complete "0.2.3"]]
   :java-source-paths ["liverepl-agent/src" "liverepl-server/src"]
   :manifest {"Agent-Class" "net.djpowell.liverepl.agent.Agent"
              "Main-Class" "net.djpowell.liverepl.client.Main"}
