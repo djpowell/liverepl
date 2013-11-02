ccabad2e7f30ec5284fa88ab391f7bb47560b67a
David Powell <djpowell@djpowell.net>
2013-11-02 10:18:58 +0000

    add tools.jar to the classpath

--- liverepl-client/project.clj
+++ liverepl-client/project.clj
@@ -1,8 +1,7 @@
 (defproject net.djpowell/liverepl-client "1.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev
-             {:resource-paths [~(.getCanonicalPath (java.io.File. (System/getProperty "java.home") "/../lib/tools.jar"))]}}
+  :plugins [[lein-jdk-tools "0.1.1"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :javac-options {:debug "true"}
